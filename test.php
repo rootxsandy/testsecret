72380d337fca8238cfdd742f07e39df9d1e8b69f
Sandeep Dhanwai <51152665+rootxsandy@users.noreply.github.com>
2025-04-27 12:38:52 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,33 @@
+# Fake secret 1 - AWS Secret Key
+AKIAIOSFODNN7EXAMPLE:abcdefghijklmnopqrstuvwxyz123456
+
+# Fake secret 2 - GitHub Token
+ghp_12345fakeGithubToken67890abcde
+
+# Fake secret 3 - Firebase Admin SDK Key
+-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgCggEApRmzdQzNSFcUKJ4BfyD4/J1cZlOf2USrhI7s6L4
+-----END PRIVATE KEY-----
+fakeFirebasePrivateKey
+
+# Fake secret 4 - PayPal API Key
+A21AAKMLrU1g6L9e_aA0z0xYZyn0qA1fz9JwxdIb58BoXt6I8ZpXQDQKmiRzcCqHkMY9D4uTYHHH9QxX8DQ
+
+# Fake secret 5 - Google Cloud API Key
+AIzaSyAZfakeGoogleAPIKey1234567890abcdef
+
+# Fake secret 6 - Slack Bot Token
+xoxb-123456789012-234567890123-ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# Fake secret 7 - Twitter API Key
+m8JKsd23HkshkL94jKsaKLxF5ahgHGFh7DkEFAKy
+
+# Fake secret 8 - Twilio API Key
+ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# Fake secret 9 - Stripe Secret Key
+sk_test_fakeStripeSecretKey_1234567890abcdefgh
+
+# Fake secret 10 - Facebook Access Token
+EAAGm0mLXQWkBA3EZB5zaReoP6wDQ5eq1rZAzr2ZCxv3zlh6zVQ0nWUg0ZA4Emd1mhfaMK2Xz4cxKHbwsXyfVVZAZBi2yAVacXZBJw51rhDczXZTmy4hbwcy9oG35mf42zL8tqGF4rrmX
+
